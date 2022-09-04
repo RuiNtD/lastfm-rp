c1d4971827129b723c7940c58788e9d88642f973
Fayne Aldan <FayneAldan@gmail.com>
2022-09-04 16:03:51 -0600

    Add player detections

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -15,5 +15,30 @@ export default dependPersist(() => (
     <SText k="username">Last.fm Username</SText>
     <SText k="appName">Application Name</SText>
     <SSwitch k="shareName">Share Username</SSwitch>
+    <SSwitch k="otherEnabled">
+      Disable when another player's Rich Presence is detected
+    </SSwitch>
+    <SSwitch k="otherListening" depends="otherEnabled">
+      "Listening to" status (Spotify)
+    </SSwitch>
+    <SSwitch k="otherCider" depends="otherEnabled">
+      <a href="https://cider.sh/" target="_blank">
+        Cider
+      </a>{" "}
+      (Apple Music)
+    </SSwitch>
+    <SSwitch k="otheriTRP" depends="otherEnabled">
+      <a href="https://itunesrichpresence.com/" target="_blank">
+        iTunes Rich Presence
+      </a>
+    </SSwitch>
+    <SSwitch k="otherAMPMD" depends="otherEnabled">
+      <a
+        href="https://premid.app/store/presences/Apple%20Music"
+        target="_blank"
+      >
+        Apple Music PreMiD
+      </a>
+    </SSwitch>
   </div>
 ));
